4b85547202f4e068dfd3da05c736c0b5ab4e725a
rlavinsh <rlavinsh@gmail.com>
2024-09-03 14:52:35 +0530

    Day 1 notes

---
+++ session-1/app.js
@@ -0,0 +1,57 @@
+// console.log("hello world");
+// variables
+
+// var let const
+
+// var a = 'amit';
+// var a = 10;
+// console.log(a);
+
+// var a;
+// console.log(a);
+
+// let b;
+// console.log(b);
+
+// Datatypes
+
+// Number
+// let a = 5;
+// let c = 5.0;
+// let d = 5.123;
+// let f = 5.1236555444;
+
+// console.log(typeof f);
+
+// String
+
+// let str = "hello world";
+// let str2 = 'g';
+
+// console.log(typeof str);
+// console.log(typeof str2);
+
+// Boolean
+
+// let isNumber = true;
+// console.log(typeof isNumber);
+
+// Array
+
+let arr =[1,2,3,"Hello","world"];
+console.log(arr[3]);
+
+let human= {
+    name: "John",
+    age: 30,
+    isMarried: true
+}
+
+// print the object value by using dot notation
+
+console.log(human.name);
+
+
+
+
+
